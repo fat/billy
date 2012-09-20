6d102c7b1cab5056949b597eb71c3a6bfe2a3601
Jacob Thornton <jacobthornton@gmail.com>
2012-09-19 17:11:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+!function () {
+
+    document.write("<div class='divCanvas'><canvas id='myCanvas' style='display: block;' width="+window.innerWidth+" height="+window.innerHeight+"><img src='sup.jpeg' width='"+window.innerWidth+"px'></canvas></div>");
+
+    document.onmousemove = moveHandler; 
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    function moveHandler (e) {
+        e = e || window.event;
+        animate(e.clientX, e.clientY);
+    }
+
+    function animate (xPos, yPos) {
+        var imgObj  = new Image();
+        var w       = window.innerWidth;
+        var h       = window.innerHeight;
+
+        imgObj = context.getImageData(0, 0, xPos, yPos);
+
+        var data         = imgObj.data;
+        var sourceWidth  = imgObj.width;
+        var sourceHeight = imgObj.height;
+
+        var destX = canvas.width / 2 - sourceWidth / 2;
+        var destY = canvas.height / 2 - sourceHeight / 2;
+
+        for (var y = 0; y < sourceHeight; y++) { 
+            for (var x = 0; x < sourceWidth; x++) {
+                data[((sourceWidth * y) + x) * 4]      = data[((sourceWidth * y) + x) * 4 + 4];
+                data[((sourceWidth * y) + x) * 4-xPos] = data[((sourceWidth * y) + x) * 4 + 1];
+                data[((sourceWidth * y) + x) * 4+xPos] = data[((sourceWidth * y) + x) * 4 + 2 - 4];
+            }
+        }
+
+        context.putImageData(imgObj, destX, destY);
+    }
+
+    window.onload = function () {
+        var imgObj = new Image();
+        imgObj.onload = function () {
+            var sourceWidth  = imgObj.width;
+            var sourceHeight = imgObj.height;
+            var destX = 0;
+            var destY = 0;
+            context.drawImage(imgObj, destX, destY);
+        }
+        imgObj.src = "sup.jpeg";
+    }
+}();
